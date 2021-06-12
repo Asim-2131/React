98d0fcdca5f8d3b92332dbbabdc1772b7591cca0
Vora Asim <asimvora2001@gmail.com>
2021-06-12 18:15:57 +0530

    intro to redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import './App.css';
 import Main from './component/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+
+const store = ConfigureStore();
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Main/>
-      </div>
-    </BrowserRouter>
+    <Provider store = {store}>
+      <BrowserRouter>
+        <div className="App">
+          <Main/>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 
 }
